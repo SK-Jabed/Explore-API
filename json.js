dca2de5cc16501b5472a3b58a73f68fe257ff143
Sheikh Jabed <jabedbd2295@gmail.com>
2024-09-30 23:44:12 +0600

    JSON JS File Completed

---
+++ json.js
@@ -0,0 +1,9 @@
+const user = {id: 1, name: "Sheikh Jabed", job: "student"};
+// JavaScript Object Notation (JSON)
+const stringified = JSON.stringify(user);
+console.log(user);
+console.log(stringified);
+/*
+* { id: 1, name: 'Sheikh Jabed', job: 'student' }
+* {"id":1,"name":"Sheikh Jabed","job":"student"} 
+*/
